37f3f72b811cacc3de38a7b453bf3520c6dff44f
Kinshuk Vasisht <kinshukvasishtsmart@gmail.com>
2022-01-03 16:41:06 +0530

    Added foreign key relation for Post entity with User

--- app/src/main/java/du/ducs/thoughtboard/data/Post.kt
+++ app/src/main/java/du/ducs/thoughtboard/data/Post.kt
@@ -2,10 +2,16 @@ package du.ducs.thoughtboard.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.util.Date
 
-@Entity(tableName = "posts")
+@Entity(tableName = "posts", foreignKeys = [ ForeignKey(
+    entity = User::class,
+    parentColumns = [ "email" ],
+    childColumns = [ "email" ],
+    onDelete = ForeignKey.CASCADE
+) ])
 data class Post(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
