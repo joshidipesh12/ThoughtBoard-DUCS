f80d180533f2f54133286490a53c3b13b7c37541
Dipesh Joshi <joshidipesh12@gmail.com>
2022-01-03 23:45:46 +0530

    updated app icon asset resolution

--- app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
+++ app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
@@ -2,6 +2,7 @@ package du.ducs.thoughtboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
