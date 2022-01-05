ddfe4bf6ff719345ba919c3cc48ca51b45f932d7
Dipesh Joshi <joshidipesh12@gmail.com>
2022-01-05 16:33:36 +0530

    added kotlin_version & MaterialComponents Theme

--- app/src/main/java/du/ducs/thoughtboard/MessageAdapter.kt
+++ app/src/main/java/du/ducs/thoughtboard/MessageAdapter.kt
@@ -15,6 +15,7 @@ class MessageAdapter(var context: Context):RecyclerView.Adapter<MessageAdapter.V
 
 
         init {
+
         }
     }
 
@@ -24,13 +25,13 @@ class MessageAdapter(var context: Context):RecyclerView.Adapter<MessageAdapter.V
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageAdapter.ViewHolder {
         TODO("Not yet implemented")
-        var view =LayoutInflater.from(parent.context).inflate(R.layout.card_layout,parent,false)
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.card_layout,parent,false)
     }
 
     override fun onBindViewHolder(holder: MessageAdapter.ViewHolder, position: Int) {
         TODO("Not yet implemented")
 
-        var data=dataList[position]
+        var data = dataList[position]
 
     }
 
