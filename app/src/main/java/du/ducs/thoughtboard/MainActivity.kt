de23860c99e9453dbf187ea29798bb7943c1819c
Mukul Singh <mukulsingh1248@gmail.com>
2022-01-06 23:52:45 +0530

    fixed splash screen

--- app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
+++ app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
@@ -2,11 +2,13 @@ package du.ducs.thoughtboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 
