3b467b3160c6be1cd97123bac58b92a3b90a784f
Mukul Singh <mukulsingh1248@gmail.com>
2022-01-07 02:23:24 +0530

    switched to firebase auth

---
+++ app/src/main/java/du/ducs/thoughtboard/FirebaseLoginActivity.kt
@@ -0,0 +1,120 @@
+package du.ducs.thoughtboard
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class FirebaseLoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val webClientId: String = BuildConfig.FIREBASE_GOOGLE_AUTH_CLIENT_ID
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_signin)
+
+        // [START config_sign_in]
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(webClientId)
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        // [END config_sign_in]
+
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // [END initialize_auth]
+
+        findViewById<Button>(R.id.signin_btn).setOnClickListener { signIn() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                Log.d(TAG, "firebase auth attempt user email:" + account.email)
+                if (checkValidEmail(account.email!!)) {
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } else {
+                    googleSignInClient.signOut()
+                    Toast.makeText(
+                        this,
+                        "Please use DUCS Domain Email to sign in",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, HomeScreenActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun checkValidEmail(email: String): Boolean {
+        return email.endsWith("@cs.du.ac.in")
+    }
+
+    companion object {
+        private const val TAG = "FirebaseLoginActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+}
--- app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
+++ app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
@@ -1,16 +1,37 @@
 package du.ducs.thoughtboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
 
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+        Log.d(TAG, "Auth user email: ${auth.currentUser?.email}")
+        if (auth.currentUser != null) {
+            startActivity(Intent(this,HomeScreenActivity::class.java))
+            finish()
+        } else {
+            Log.d(TAG, "Starting Sign-in Process")
+            startActivity(Intent(this,FirebaseLoginActivity::class.java))
+            finish()
+        }
     }
 
-
+    companion object {
+        val TAG = "MainActivity"
+    }
 }
