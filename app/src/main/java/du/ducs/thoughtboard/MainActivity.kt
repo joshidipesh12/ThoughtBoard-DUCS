b2754993a1d8b6cb0bc98ccee3ad4796b2c9dc43
Kinshuk Vasisht <kinshukvasishtsmart@gmail.com>
2022-01-07 23:27:04 +0530

    Setup basic navigation

--- app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
+++ app/src/main/java/du/ducs/thoughtboard/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -12,6 +15,7 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +25,23 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
         Log.d(TAG, "Auth user email: ${auth.currentUser?.email}")
+
         if (auth.currentUser != null) {
-            // TODO(Setup Navigation here)
-//            startActivity(Intent(this,MainActivity2::class.java))
-            finish()
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            navController = navHostFragment.navController
+
+            // FIXME: Decide how to setup navController with the activity's non-existent action bar.
+            // setupActionBarWithNavController(navController)
         } else {
             Log.d(TAG, "Starting Sign-in Process")
             startActivity(Intent(this,FirebaseLoginActivity::class.java))
             finish()
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     companion object {
