e8cfc38710bd330732b1769c3ce50e31a5dd17c7
Dipesh Joshi <joshidipesh12@gmail.com>
2022-01-07 10:20:52 +0530

    converted HomeScreen activity to fragment bohot mehnat lgi 😩😩😩

---
+++ app/src/main/java/du/ducs/thoughtboard/HomeScreenFragment.kt
@@ -0,0 +1,119 @@
+package du.ducs.thoughtboard
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.*
+import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import du.ducs.thoughtboard.adapter.ItemAdapter
+import du.ducs.thoughtboard.data.DummyDataSource
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class HomeScreenFragment : Fragment(), DatePicker.OnDateChangedListener,
+    DatePickerDialog.OnDateSetListener, RecyclerView.OnItemTouchListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
+
+        //This is just for now when we will work with data we'll set it to last date of data
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+        sdf.applyPattern("EEEE, dd MMM yyyy")
+        val sMyDate: String = sdf.format(Calendar.getInstance().time)
+        (activity as AppCompatActivity?)!!.supportActionBar?.title = sMyDate
+
+        // Initialize data.
+        val myDataset = DummyDataSource().loadDummyMessages()
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = object : GridLayoutManager((activity as AppCompatActivity?)!!, 2){
+            override fun checkLayoutParams(lp: RecyclerView.LayoutParams) : Boolean {
+                lp.height = (view.width / 2.3).toInt()
+                return true
+            }
+        }
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.layout_home_screen_menu, menu)
+        super.onCreateOptionsMenu(menu,inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        showDatePickerDialog()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showDatePickerDialog()  {
+        val datePickerDialog = DatePickerDialog(
+            (activity as AppCompatActivity?)!!,
+            this,
+            Calendar.getInstance().get(Calendar.YEAR),
+            Calendar.getInstance().get(Calendar.MONTH),
+            Calendar.getInstance().get(Calendar.DAY_OF_MONTH) )
+
+        datePickerDialog.show()
+    }
+
+    override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year,monthOfYear,dayOfMonth)
+
+        val sdf = SimpleDateFormat("EEEE, dd MMM yyyy", Locale.ENGLISH)
+        val sMyDate: String = sdf.format(calendar.time)
+
+        (activity as AppCompatActivity?)!!.supportActionBar?.title = sMyDate
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+
+        val calendar = Calendar.getInstance()
+        calendar.set(year,month,dayOfMonth)
+
+        val sdf = SimpleDateFormat("EEEE, dd MMM yyyy", Locale.ENGLISH)
+        val sMyDate: String = sdf.format(calendar.time)
+
+        (activity as AppCompatActivity?)!!.supportActionBar?.title = sMyDate
+
+    }
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        // try avoiding this implementation
+        return true
+    }
+
+    // item click listener
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+        // try avoiding this implementation
+    }
+
+}
--- app/src/main/java/du/ducs/thoughtboard/MessageScreenActivity.kt
+++ app/src/main/java/du/ducs/thoughtboard/MessageScreenActivity.kt
@@ -8,10 +8,6 @@ import du.ducs.thoughtboard.databinding.ActivityMessageScreenBinding
 
 class MessageScreenActivity : AppCompatActivity() {
 
-    val dummy_title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque sed egestas aliquam"
-    val dummy_message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porta quis mauris metus, vel ut amet sed nunc. Facilisis est sit volutpat hac platea purus elementum, id. Aliquam morbi lobortis augue viverra. Eget velit venenatis tortor purus rhoncus metus.\n" +
-            "Eu suspendisse libero potenti nunc aliquam. Ac fermentum et viverra tellus risus, nunc. Ultrices sed venenatis eget dui mauris nulla quis fermentum. Mattis ac arcu tristique praesent cum feugiat gravida et ut. Egestas ullamcorper mauris fringilla et interdum feugiat pretium erat volutpat. Tristique purus morbi non pharetra orci. Vitae, risus mauris pretium tortor in consectetur. Tellus sem lectus dui orci facilisis amet."
-    val dummy_name = "Rishi Sharma"
     val dummy_email = "rishi.sharma115@gmail.com"
 
     private lateinit var binding: ActivityMessageScreenBinding
@@ -22,9 +18,9 @@ class MessageScreenActivity : AppCompatActivity() {
         binding = ActivityMessageScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.messageTitle.text = dummy_title
-        binding.messageAuthor.text = dummy_name
-        binding.messageContent.text = "$dummy_message $dummy_message"
+        binding.messageTitle.text = R.string.dummy_title.toString()
+        binding.messageAuthor.text = R.string.dummy_author.toString()
+        binding.messageContent.text = "${R.string.dummy_message} ${R.string.dummy_message}"
 
         // change visibility if post.email == user.email
         binding.emailButton.visibility = View.VISIBLE
